b1f643836a650264a5145dab5f3254165470fae6
Aarini123 <77624143+Aarini123@users.noreply.github.com>
2021-03-15 16:36:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,13 +21,14 @@ function add_item(){
     var item= document.getElementById("add_item").value;
     menu_list_array.push(item);
     menu_list_array.sort();
-    htmldata="<section  class='cards'>";
-    for(var i=0;1<menu_list_array.length;i++ ){
-        htmldata=htmldata+'<div class"card"' + '<img src="pizzaImg.png"' + menu_list_array[i] + '</div>'
+    htmldata="<section  class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++ ){
+        htmldata=htmldata+'<div class="card">' + '<img src="pizzaImg.png">' + menu_list_array[i] + '</div>'
    
 }
 htmldata=htmldata + "</section>";
-document.getElementById("display_addmenu").innerHTML=htmldata;
+console.log(htmldata);
+document.getElementById("display_addedmenu").innerHTML=htmldata;
 
 
 }
